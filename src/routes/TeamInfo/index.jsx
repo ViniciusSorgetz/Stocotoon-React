950b7c58814dfbe589b0cccfe2038b08ef466270
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-09-06 15:47:34 -0300

    Avanço na página de informações da equipe. Falta adicionar funcionalidades

--- src/routes/TeamInfo/index.jsx
+++ src/routes/TeamInfo/index.jsx
@@ -3,10 +3,15 @@ import { useParams } from 'react-router-dom';
 import stocotoonAPI from '../../axios/config';
 import { UserContext } from '../../contexts/User';
 
+import TeamPhoto from '../../assets/TeamPhoto.svg';
+import Options from '../../assets/Options.svg';
+import MemberPhoto from '../../assets/MemberPhoto.svg';
+import './TeamInfo.css';
+
 const TeamInfo = () => {
 
     const {session, setSession } = useContext(UserContext);
-    const [teamInfo, setTeamInfo] = useState({});
+    const [teamInfo, setTeamInfo] = useState(null);
     const { TeamId } = useParams();
 
     useEffect(() => {
@@ -19,6 +24,7 @@ const TeamInfo = () => {
                 Authorization: `Bearer ${session.UserToken}`
             }});
             setTeamInfo(resp.data);
+            console.log(resp.data);
         } 
         catch (error) {
             console.log(error);
@@ -26,18 +32,75 @@ const TeamInfo = () => {
     }
 
     return (
-        <div className="container">
-            <div className="row mt-nav">
-                <div className="col-4">
-                    <h1 className="text-light">{teamInfo.name}</h1>
+        <div className="container w-75">
+            {teamInfo && 
+            <div className="row mt-nav gap-5">
+                <div className="col-12 col-lg-6 mb-0 mb-5">
+                    <div className="d-flex justify-content-between align-items-start mb-3">
+                        <div className="d-flex w-100 justify-content-center">
+                            <img src={TeamPhoto}/>
+                        </div>
+                        <img src={Options} style={{scale: "0.8", cursor: "pointer"}}/>
+                    </div>
+                    <h1 className="font-grand text-cyan font-bold">{teamInfo.name}</h1>
+                    {teamInfo.description ? 
+                        <p className="text-light mb-5">{teamInfo.description}</p> :
+                        <p className="text-light mb-5">Insira a descrição da equipe aqui.</p>
+                    }
+                    <div className="row d-flex">
+                        <p className="text-light">Esta equipe contém:</p>
+                        <div className="col-4">
+                            <h3 className="text-light font-bold text-center h2">
+                                {String(teamInfo.amount.stories).padStart(2, "0")}
+                            </h3>
+                            <p className="text-light text-center">Histórias</p>
+                        </div>
+                        <div className="col-4">
+                            <h3 className="text-light font-bold text-center h2">
+                                {String(teamInfo.amount.chapters).padStart(2, "0")}
+                            </h3>
+                            <p className="text-light text-center">Capítulos</p>
+                        </div>
+                        <div className="col-4">
+                            <h3 className="text-light font-bold text-center h2">
+                                {String(teamInfo.amount.pages).padStart(2, "0")}
+                            </h3>
+                            <p className="text-light text-center">Páginas</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-4">
-                    <h1 className="text-light">B</h1>
-                </div>
-                <div className="col-4">
-                    <h1 className="text-light">C</h1>
+                <div className="members-container col-12 col-lg-5">
+                    <div className="d-flex justify-content-between">
+                        <h3 className="font-grand text-cyan font-bold">Integrantes da equipe</h3>
+                        <img src={Options} style={{scale: "0.8", cursor: "pointer"}}/>
+                    </div>
+                    <p className="text-light font-grand font-bold lead">Dono</p>
+                    <ul>
+                        {teamInfo.members.owners.map(owner => (
+                            <li className="d-flex gap-2">
+                                <img src={MemberPhoto}/>
+                                <div className="d-flex flex-column">
+                                    <span className="text-light font-bold">{owner.name}</span>
+                                    <span className="text-light">dono</span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <p className="text-light font-grand font-bold lead">Membros</p>
+                    <ul>
+                        {teamInfo.members.members.map(member => (
+                            <li className="d-flex gap-2">
+                                <img src={MemberPhoto}/>
+                                <div className="d-flex flex-column">
+                                    <span className="text-light font-bold">{member.name}</span>
+                                    <span className="text-light">membro</span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
+            }
         </div>
     )
 }
